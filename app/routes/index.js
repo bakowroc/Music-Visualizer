2c34b9aec009446520f79807bd411a674d01dff2
bakowroc <kontakt@maciejbakowicz.pl>
2017-06-30 14:45:23 +0200

    Merge

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-const IndexController = require('../controllers/index.controller');
-
-module.exports = function(app){
-
-    app.get('/', IndexController)
-
-=======
 
 const IndexController = require('../controllers/index.controller');
 
 
 module.exports = function(app){
   app.get('/', IndexController);
->>>>>>> b0e410a647699f5f08b7bdf8a98d578ad354c911
 }
